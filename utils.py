e90c6f803bc07f943e281a227d4f202ed09925fb
Rechu <reszkettle@gmail.com>
2021-11-09 13:39:56 +0100

    Added raising an exception when there is a problem with external api

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ from schemas import InAnalyseString
 import requests as req
 from settings import EXTERNAL_ENDPOINT
 from dicttoxml import dicttoxml
+from fastapi import HTTPException, status
 
 
 def csv_string_from_dict(data: dict) -> str:
@@ -27,3 +28,8 @@ def process_request(request: InAnalyseString) -> str:
         elif request.format == 'csv':
             return csv_string_from_dict(data)
         return data
+
+    raise HTTPException(
+        status_code=502,
+        detail="External API error"
+    )
